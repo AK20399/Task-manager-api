d74c7d2b718c66133fb8521ca068c08d32b14d5f
Aakash Darji <aakash.darji20@gmail.com>
2021-12-18 17:27:10 +0530

    fixes

--- src/Common/email.js
+++ src/Common/email.js
@@ -1,22 +1,25 @@
-const sendGrid = require('@sendgrid/mail')
-sendGrid.setApiKey(process.env.SEND_GRID_API_KEY)
+/* eslint-disable */
+// const sendGrid = require('@sendgrid/mail')
+// sendGrid.setApiKey(process.env.SEND_GRID_API_KEY)
 
 const sendWelcomeEmail = (name, email) => {
-    sendGrid.send({
-        to: email,
-        from: 'aakash.darji@kevit.io',
-        subject: "Thanks for joining in",
-        text: `Welcome to the app ${name}.`,
-    })
+    // sendGrid.send({
+    //     to: email,
+    //     from: 'aakash.darji@kevit.io',
+    //     subject: "Thanks for joining in",
+    //     text: `Welcome to the app ${name}.`,
+    // })
+    return 'a'
 }
 
 const sendCancellationEmail = (name, email) => {
-    sendGrid.send({
-        to: email,
-        from: 'aakash.darji@kevit.io',
-        subject: "WHY WHY WHY",
-        text: `You ${name}. WHY DID YOU CANCEL OUR SERVICE. WHY WHY WHY?????? `,
-    })
+    // sendGrid.send({
+    //     to: email,
+    //     from: 'aakash.darji@kevit.io',
+    //     subject: "WHY WHY WHY",
+    //     text: `You ${name}. WHY DID YOU CANCEL OUR SERVICE. WHY WHY WHY?????? `,
+    // })
+    return 'a'
 }
 
 module.exports = {
--- src/Common/response.js
+++ src/Common/response.js
@@ -1,6 +1,6 @@
-const response = (res, message = '', data = {}, status = 200) => {
-    return res.status(status).json({ message, data })
-}
+const response = (res, message = '', data = {}, status = 200) =>
+    res.status(status).json({ message, data })
+
 
 module.exports = {
     response
